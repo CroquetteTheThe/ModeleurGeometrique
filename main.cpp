331f3129fb8af8202b1a283383ffa7e2f9e2b1d8
Orval Touitou <orval.touitou@gmail.com>
2017-10-03 00:19:37 +0200

    Added the shape reader

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include "Window.h"
-#include "Color.h"
 #include "Shape.h"
+#include "OFFReader.h"
 
 int main(int argc, char **argv) {
 	auto window = new Window(&argc, argv, "TP1 - Orval Touitou", 600, 600);
-
-	auto shape = new Shape(Color::purple);
-	shape->addPoint({0, 0, 0});
-	shape->addPoint({1, 0, 0});
-	shape->addPoint({0.5, 1, 0});
-	shape->addFace({1, 2, 3});
-	window->add(shape);
+	auto reader = OFFReader();
+	window->add(reader.fromFile("../resources/teapot.off"));
 	window->show();
 	return EXIT_SUCCESS;
 }
